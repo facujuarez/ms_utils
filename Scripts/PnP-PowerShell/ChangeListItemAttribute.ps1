8866414fea1e9ca1b80f8e22afc5921ee6a6b924
Facundo Juárez <facu_juarez.10@outlook.com>
2023-12-18 12:16:44 -0300

    ChangeListItemAttribute OK

---
+++ Scripts/PnP-PowerShell/ChangeListItemAttribute.ps1
@@ -0,0 +1,47 @@
+$developTenant = "facujuarezdev"
+$developSiteAlias = "GestionDocumentalDev"
+
+$tenantName = $developTenant
+$siteAlias = $developSiteAlias
+$siteUrl = "https://${tenantName}.sharepoint.com/sites/${siteAlias}"
+
+Write-Host
+Write-Host "Iniciando script..." -ForegroundColor Magenta
+Write-Host
+
+Write-Host "Conectando al sitio ${siteAlias}..." -ForegroundColor Yellow
+try {
+    # Conecta a SharePoint
+    Connect-PnPOnline -Url $siteUrl -Interactive -ErrorAction Stop
+}
+catch {
+    Write-Host "Error al conectarse a SharePoint Online" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
+Write-Host "Actualizando propiedades del documento..." -ForegroundColor Yellow
+try {
+    # Obtiene el elemento de lista
+    $item = Get-PnPListItem -List "Test" -ID 1
+    Write-Host $item.FieldValues
+
+    # Establece la nueva fecha de creación
+    $newCreatedDate = (Get-Date -Year 2023 -Month 07 -Day 20)
+    Write-Host $newCreatedDate
+
+    # Actualiza el elemento de lista
+    Set-PnPListItem -List "Test" -Identity 1 -Values @{"Created" = $newCreatedDate; }
+
+    Write-Host "Propiedades actualizadas correctamente." -ForegroundColor Green
+}
+catch {
+    Write-Host "Error al conectarse a SharePoint Online" -ForegroundColor Red
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
+Write-Host
+Write-Host "Fin ejecución del script." -ForegroundColor Magenta
+Write-Host
+
+
+
